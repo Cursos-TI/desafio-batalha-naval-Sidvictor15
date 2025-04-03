729868c6a8df0187bc91af4696a545e620593187
Sidvictor15 <sidneyvictor52@gmail.com>
2025-04-03 18:50:25 +0000

    comit.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,40 +2,57 @@
 
 int main() {
     char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    char tabuleiro[10][10]; // Matriz para representar o tabuleiro
+    char tabuleiro[10][10]; 
 
-    // Inicializa o tabuleiro com '0'
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             tabuleiro[i][j] = '0';
         }
     }
 
-    // Posiciona um navio (exemplo: de 3 células na linha 6, colunas D a F)
-    for (int j = 3; j < 6; j++) {
-        tabuleiro[5][j] = '3'; // 'X' representa uma parte do navio
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (i == j)
+            {
+                tabuleiro[i][j] = '3';   
+            }
+            
+        }
+        
     }
 
-    for (int i = 2; i < 5; i++)
+    for (int i = 0; i < 10; i++)
     {
-       tabuleiro[i][8] = '3';
+        for (int j = 0; j < 10; j++)
+        {
+            if (i + j == 9)
+            {
+                tabuleiro[i][j] = '3';   
+            }
+            
+        }
+        
     }
     
 
+
+
+
+    // Exibe o tabuleiro
     printf("TABULEIRO BATALHA NAVAL\n\n");
 
-    // Imprime os cabeçalhos das colunas (A a J)
-    printf("   "); // Espaço para alinhar com os números à esquerda
+    printf("   "); 
     for (int j = 0; j < 10; j++) {
         printf(" %c ", letras[j]); 
     }
     printf("\n");
 
-    // Imprime o tabuleiro com numeração das linhas
     for (int i = 0; i < 10; i++) {
-        printf("%2d ", i + 1); // Garante alinhamento correto para números de 1 a 10
+        printf("%2d ", i + 1); 
         for (int j = 0; j < 10; j++) {
-            printf(" %c ", tabuleiro[i][j]); // Agora imprime os valores corretos da matriz
+            printf(" %c ", tabuleiro[i][j]); 
         }
         printf("\n");
     }
